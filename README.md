d6501652e091fd7d1b4559b5dd4bf6281a692567
Ionut Malutan <ionut.malutan@darkhound.ro>
2020-06-06 09:34:05 +0300

    Updated for redmine version 4.1.1.stable
    Added administration icons for RedmineUP contacts and finances plugins
    Updated screenshots
    Some minor mobile styling

---
+++ README.md
@@ -0,0 +1,30 @@
+# redmine theme #
+Modern Redmine Theme.
+This is based on the work done by yenihayat. Thank you.
+
+His work can be accessed at https://github.com/yenihayat/redmine-theme-yh
+
+## Installation
+
+* Download and unzip latest release or just clone the project into your Redmine themes directory (e.g. ` ../public/themes/` or if you're using **redmine** packages from Debian repos use `/usr/share/redmine/public/themes` directory)
+* Restart Redmine to make the newly installed theme available in your theme list.
+* Go to `"Administration / Settings / Display"` and choose this theme (redmine-theme-yh) in themes list.
+* Save your changes.
+
+## Contributing
+1. Fork it
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Add some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create new Pull Request
+
+## Screenshots
+![](https://raw.githubusercontent.com/imalutan/redmine-theme/master/screenshots/activity.png)
+___
+![](https://raw.githubusercontent.com/imalutan/redmine-theme/master/screenshots/admin.png)
+___
+![](https://raw.githubusercontent.com/imalutan/redmine-theme/master/screenshots/gantt.png)
+___
+![](https://raw.githubusercontent.com/imalutan/redmine-theme/master/screenshots/issue-detail.png)
+___
+![](https://raw.githubusercontent.com/imalutan/redmine-theme/master/screenshots/issues.png)
---
+++ javascripts/theme.js
@@ -0,0 +1,60 @@
+
+function createBtn() {
+	$("input[type=submit]").addClass("btn btn-sm btn-primary");
+	$("#attach_files_link").addClass("btn btn-sm btn-primary");
+	$(".tab-content p a.icon").addClass("btn btn-sm btn-primary");
+
+        $("a.icon.icon-zoom-in").addClass("btn btn-sm btn-outline-dark");
+        $("a.icon.icon-zoom-out").addClass("btn btn-sm btn-outline-dark");
+        $(".contextual a.icon.icon-settings").addClass("btn btn-sm btn-outline-dark");
+        $(".contextual a.icon.icon-del").addClass("btn btn-sm btn-outline-dark");
+
+	$("#login-submit").addClass("btn btn-sm btn-primary");
+	$("input[type=submit] + a.icon").addClass("btn btn-sm btn-outline-dark");
+	$(".controller-enumerations.action-index #content p > a.icon").addClass("btn btn-sm btn-primary");
+	$("#ajax-modal input[type=button]").addClass("btn btn-sm btn-outline-dark");
+	$("#content").children(".contextual").find("a:first").addClass("btn btn-sm btn-primary");
+	$("#content").children(".contextual").find("a:not(:first):not(.lock)").addClass("btn btn-sm btn-outline-dark");
+	
+	$(".issue .description .contextual a").addClass("btn btn-sm btn-outline-dark");
+	$(".issue #relations .contextual a, .issue #issue_tree .contextual a").addClass("btn btn-sm btn-outline-dark");
+
+	$("#content .contextual a.lock").addClass("btn btn-sm btn-danger");
+	$(".news.box a:last").addClass("btn btn-sm btn-outline-dark");
+	$("p.buttons a").addClass("btn btn-sm btn-outline-dark");
+	$(".icon-fav-off").addClass("btn btn-sm btn-outline-dark");
+	$(".icon-fav").addClass("btn btn-sm btn-outline-dark");
+	$("#activity_scope_form").find("p input").addClass("btn btn-sm btn-dark");
+	$("table.query-columns .buttons input[type=button]").addClass("btn btn-sm btn-outline-dark");
+}
+
+function createDOMObserver(targetNode, callback) {
+	if (targetNode) {
+		const config = { attributes: true, childList: true, subtree: true };
+		const observer = new MutationObserver(callback);
+		observer.observe(targetNode, config);	
+	}
+}
+
+function init() {
+	$("table").wrap("<div class='table'></div>");
+	$( "#ajax-modal" ).on( "dialogopen", function( event, ui ) {
+		createBtn();	
+	});
+	createBtn();
+	createDOMObserver(document.querySelectorAll('body')[0], (mutationsList, observer) => {
+		for(var mutation of mutationsList) {
+        if (mutation.type == 'attributes') {
+          $("#member-add-submit").addClass("btn btn-sm btn-primary");
+	  $("#member-add-submit + input").addClass("btn btn-sm btn-outline-dark");
+	  $("icon.icon-fav").addClass("btn btn-sm btn-outline-dark");
+	  $("icon.icon-fav-off").addClass("btn btn-sm btn-outline-dark");
+        }
+    }
+	});
+}
+
+
+$(document).ready(function() {
+	init();
+});
